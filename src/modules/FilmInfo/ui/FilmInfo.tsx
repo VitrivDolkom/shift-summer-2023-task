81352f6e4984a7425c76fc96ed7e51c70e330d52
Volkov Dmitriy <vdy0555@gmail.com>
2023-07-04 23:25:07 +0700

    started film info layout

--- src/modules/FilmInfo/ui/FilmInfo.tsx
+++ src/modules/FilmInfo/ui/FilmInfo.tsx
@@ -1,6 +1,8 @@
 import { useAppDispatch, useAppSelector } from '@/store'
 import { useEffect } from 'react'
 
+import { FilmRating } from '@/shared/uikit/FilmRating'
+
 import { type FilmInfoProps } from '../lib/types'
 import { fetchFilmInfo } from '../model/thunk'
 
@@ -20,9 +22,24 @@ export const FilmInfo = ({ id }: FilmInfoProps) => {
     return <h1 className={s.title}>Pending...</h1>
   }
 
-  if (request.status === 'error') {
+  if (request.status === 'error' || film === undefined) {
     return <h1>No such film</h1>
   }
 
-  return <h1 className={s.title}>{film?.name || 'dima'}</h1>
+  return (
+    <div className={s.wrapper}>
+      <div className={s.left}>
+        <img src={film.img} alt="картинка" />
+        <div className={s.watchDate}>в прокате</div>
+        <div className={s.watchDate}></div>
+      </div>
+      <div className={s.right}>
+        <div className={s.name}></div>
+        <div className={s.director}></div>
+        <div className={s.mainInfo}></div>
+        <FilmRating rating={film.userRatings.kinopoisk} />
+        <div className={s.description}>{film.description}</div>
+      </div>
+    </div>
+  )
 }
