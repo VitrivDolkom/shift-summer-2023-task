820c41a0538937a1679922f5815a5b83e0445b3c
Volkov Dmitriy <vdy0555@gmail.com>
2023-07-05 17:10:57 +0700

    parted ui and logic in film schedule module

--- src/modules/FilmInfo/ui/FilmInfo.tsx
+++ src/modules/FilmInfo/ui/FilmInfo.tsx
@@ -1,15 +1,9 @@
 import { useAppDispatch, useAppSelector } from '@/store'
 import { useEffect } from 'react'
 
-import { FilmRating } from '@/shared/uikit/FilmRating'
-
-import { FilmDirectors } from '../components/FilmDirectors'
-import { FIlmGenreCountryYear } from '../components/FIlmGenreCountryYear'
-import { WatchDate } from '../components/WatchDate'
 import { type FilmInfoProps } from '../lib/types'
 import { fetchFilmInfo } from '../model/thunk'
-
-import s from './styles.module.css'
+import { FilmInfoComponent } from './FilmInfoComponent'
 
 export const FilmInfo = ({ id }: FilmInfoProps) => {
   const dispatch = useAppDispatch()
@@ -22,26 +16,12 @@ export const FilmInfo = ({ id }: FilmInfoProps) => {
   }, [])
 
   if (request.status === 'pending') {
-    return <h1 className={s.title}>Pending...</h1>
+    return <h1>Pending...</h1>
   }
 
   if (request.status === 'error' || film === undefined) {
     return <h1>No such film</h1>
   }
 
-  return (
-    <div className={s.wrapper}>
-      <div className={s.left}>
-        <img src={`${import.meta.env.VITE_BACKEND_URL}${film.img}`} alt="картинка" />
-        <WatchDate />
-      </div>
-      <div className={s.right}>
-        <div className={s.name}>{film.name}</div>
-        <FilmDirectors directors={film.directors} />
-        <FIlmGenreCountryYear genres={film.genres} country={film?.country} date={film.releaseDate} />
-        <FilmRating rating={film.userRatings.kinopoisk} />
-        <div className={s.description}>{film.description}</div>
-      </div>
-    </div>
-  )
+  return <FilmInfoComponent film={film} />
 }
