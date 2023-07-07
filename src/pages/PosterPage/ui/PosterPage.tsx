9747f5ee6b639af4cf3fd16532848da26ec763e9
Volkov Dmitriy <vdy0555@gmail.com>
2023-07-07 18:28:37 +0700

    fixed linter warnging

--- src/pages/PosterPage/ui/PosterPage.tsx
+++ src/pages/PosterPage/ui/PosterPage.tsx
@@ -4,17 +4,15 @@ import { PosterFilms } from '@/modules/PosterFilms'
 import posterTitle from './img/posterTitle.svg'
 import s from './styles.module.css'
 
-export const PosterPage = () => {
-  return (
-    <div>
-      <Header type="withButton" />
-      <main>
-        <div className={s.title}>
-          <img src={posterTitle} alt="афиша" />
-          <h1 className={s.subtitle}> на сегодня</h1>
-        </div>
-        <PosterFilms />
-      </main>
-    </div>
-  )
-}
+export const PosterPage = () => (
+  <div>
+    <Header type="withButton" />
+    <main>
+      <div className={s.title}>
+        <img src={posterTitle} alt="афиша" />
+        <h1 className={s.subtitle}> на сегодня</h1>
+      </div>
+      <PosterFilms />
+    </main>
+  </div>
+)
