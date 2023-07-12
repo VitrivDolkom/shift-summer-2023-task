e7371e9f39ea06dbf08e6bf33e775b1830c50c25
Volkov Dmitriy <vdy0555@gmail.com>
2023-07-12 15:56:24 +0700

    cleaned deprecated types

--- src/modules/FilmSchedule/api/FilmScheduleService.service.ts
+++ src/modules/FilmSchedule/api/FilmScheduleService.service.ts
@@ -1,7 +1,5 @@
 import { instance } from '@/shared/api'
 
-import { FilmScheduleResponse } from '../model/types'
-
 export const FilmScheduleService = {
-  getFilmSchedule: async (id: string) => await instance.get<FilmScheduleResponse>(`/film/${id}/schedule`)
+  getFilmSchedule: async (id: string) => await instance.get<api.ScheduleResponse>(`/film/${id}/schedule`)
 }
